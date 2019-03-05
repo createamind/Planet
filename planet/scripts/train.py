154bd285624bbebb76b66efb6722f13c6f41e141
liu <supernova.bin@gmail.com>
2019-03-05 20:19:48 +0800

    carla9.4

--- planet/scripts/train.py
+++ planet/scripts/train.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
   boolean = lambda x: bool(['False', 'True'].index(x))
   parser = argparse.ArgumentParser()
   parser.add_argument(
-      '--logdir', default='./logcarla4')
+      '--logdir', default='./log1')
   parser.add_argument(
       '--num_runs', type=int, default=1)
   parser.add_argument(
